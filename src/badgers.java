88c709b4cdfc783c137799820acf377e5df01de8
ashlynaguirre <league@mini222.local>
2016-03-02 14:58:59 -0800

    a tad more

---
+++ src/badgers.java
@@ -0,0 +1,15 @@
+
+public class badgers {
+public static void main(String[] args) {
+	for (int k = 0; k < 3; k++) {
+		
+	
+	for (int i = 0; i < 12; i++) {
+		System.out.println("badger"); }
+		for (int j = 0; j < 2; j++){
+	System.out.println("mushroom");
+			
+		
+		}}
+}
+}
---
+++ src/loop.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class loop {
+public static void main(String[] args) {
+	for (int i = 0; i < 16; i++) {
+		JOptionPane.showMessageDialog(null, "when I was "+ i);
+
+	}
+}
+}
